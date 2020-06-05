842b08f94687a4a311d5143699d2d3afafc92779
Dmitry Sychev <axiumone@gmail.com>
2020-06-05 21:51:14 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr, ele)
+  arr.insert(ele)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(Array)
+  array.flatten
+end
+
+def using_delete(arr, str)
+  arr.delete(str)
+end
